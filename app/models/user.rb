ccce83e060ffc0c96ab732307394435ba2ae90ca
Tim Baker <timpwbaker@gmail.com>
2017-09-26 16:34:29 +0100

    Users can like shouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,20 @@ class User < ApplicationRecord
   include Clearance::User
 
   has_many :shouts, dependent: :destroy
+  has_many :likes
+  has_many :liked_shouts, through: :likes, source: :shout
 
   validates :username, presence: true, uniqueness: true
+
+  def like(shout)
+    liked_shouts << shout
+  end
+
+  def liked?(shout)
+    liked_shout_ids.include?(shout.id)
+  end
+
+  def unlike(shout)
+    liked_shouts.destroy(shout)
+  end
 end
